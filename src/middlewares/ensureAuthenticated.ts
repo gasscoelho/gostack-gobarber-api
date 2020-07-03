2532f02df6c9031fa6612cf8b314d7a3baf1d884
Gabriel Coelho <gaaucocoelho@gmail.com>
2020-07-03 14:43:05 -0300

    feat: add authentication middleware

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
+import AppError from '../errors/AppError';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  const authHeader = req.headers.authorization;
+
+  // Check if token was passed to th headers
+  if (!authHeader) {
+    throw new AppError('JWT token is missing', 401);
+  }
+
+  // Bearer <token>
+  const [, token] = authHeader.split(' ');
+
+  const { secret } = authConfig.jwt;
+
+  try {
+    if (secret) {
+      const decoded = verify(token, secret) as TokenPayload;
+      const { sub } = decoded;
+      req.user = {
+        id: sub,
+      };
+
+      return next();
+    }
+    throw new AppError(`Couldn't find JWT secret`, 500);
+  } catch {
+    throw new AppError('Invalid JWT token', 401);
+  }
+}
