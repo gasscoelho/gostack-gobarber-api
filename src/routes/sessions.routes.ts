ae3c0352b5e6c4fdff48397241cee1cdecda1d9b
Gabriel Coelho <gaaucocoelho@gmail.com>
2020-07-03 14:46:24 -0300

    feat: add application routes
    routes added:
    - appointments
    - session
    - users

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (req, res) => {
+  const { email, password } = req.body;
+
+  const authenticateUser = new AuthenticateUserService();
+
+  const { user, token } = await authenticateUser.execute({ email, password });
+
+  // Delete password field from user object
+  delete user.password;
+
+  return res.json({ user, token });
+});
+
+export default sessionsRouter;
