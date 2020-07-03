77c7a5bfdf77278f356f85159e4dcb4372283ffd
Gabriel Coelho <gaaucocoelho@gmail.com>
2020-07-03 14:43:37 -0300

    feat: add Appointments and User models

---
+++ src/models/User.ts
@@ -0,0 +1,32 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar')
+  name: string;
+
+  @Column('varchar')
+  email: string;
+
+  @Column('varchar')
+  password: string;
+
+  @Column('varchar')
+  avatar: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+export default User;
