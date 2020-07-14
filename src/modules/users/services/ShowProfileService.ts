074435dade3719c09f88b62fb158cb709f9b58ca
Gabriel Coelho <gaaucocoelho@gmail.com>
2020-07-14 02:55:33 -0300

    feat: add Profile User functionalitties
    - Services
    - Controllers
    - Routes

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/errors/AppError';
+
+import { injectable, inject } from 'tsyringe';
+
+import User from '../infra/typeorm/entities/User';
+
+import IUsersRepository from '../repositories/IUserRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+class ShowProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError(`Couldn't find an User`);
+    }
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
